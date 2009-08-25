6e476294165a5c846f5ac06976a27148e7116b04
Yehuda Katz + Carl Lerche <ykatz+clerche@engineyard.com>
2009-08-24 17:48:18 -0700

    Step 1: We're going to extract the authorization into a middleware; let's test it first.

---
+++ test/integration/middleware_test.rb
@@ -0,0 +1,35 @@
+require File.join(File.dirname(__FILE__), '..', 'test_helper')
+
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
+class MiddlewareTest < ActionController::IntegrationTest
+  def setup
+    User.delete_all
+    User.create(:name => "dave", :password => "secret", :password_confirmation => "secret")
+  end
+
+  # Replace this with your real tests.
+  test "login does return a 401" do
+    visit "/admin/login"
+    assert_response :success
+  end
+  
+  test "other URLs return a 401" do
+    visit "/admin"
+    assert_response :unauthorized
+    assert_have_selector "p:contains('Access denied')"
+  end
+
+  test "if the user is :logged_out, redirect to /admin/login" do
+    basic_auth "dave", "secret"
+    visit "/admin"
+    visit "/admin/logout"
+
+    visit "/admin"
+    assert_equal current_url, "/admin/login"
+  end
+end
